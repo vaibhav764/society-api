e67c131d349534f0b7391243c1cc2ad4c76cd49d
unknown <mahesh.rane@softonauts.com>
2019-09-30 12:55:39 +0530

    chane in get_all_company

--- application/controllers/Admin_api.php
+++ application/controllers/Admin_api.php
@@ -480,6 +480,16 @@ class Admin_api extends CI_Controller {
 							unset($_POST['select']);
 						}
 						$company = $this->model->getData('company',['created_by'=>$_POST['created_by']],$select);
+						if(!empty($company))
+					{
+						foreach ($company as $key => $value) {
+
+							$company[$key]['country_name']=$this->model->getValue('countries','name',['id'=>$value['country_id']]);
+							$company[$key]['city_name']=$this->model->getValue('cities','city',['id'=>$value['city_id']]);
+							$company[$key]['state_name']=$this->model->getValue('states','name',['id'=>$value['state_id']]);
+						}
+					}
+
 						$response['next_id'] = $this->model->generate_next_id('company','autoid','com','3');
 						$response['companies'] = $company;
 						$response['message'] = 'success';
